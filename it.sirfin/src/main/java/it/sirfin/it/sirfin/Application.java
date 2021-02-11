84cf75a439f4285c7f84927e2d538c012e84f45d
Matteo Moretti <78532093+matteo2025@users.noreply.github.com>
2021-02-11 14:26:38 +0100

    creazione spring

--- it.sirfin/src/main/java/it/sirfin/it/sirfin/Application.java
+++ it.sirfin/src/main/java/it/sirfin/it/sirfin/Application.java
@@ -1,13 +1,13 @@
-package it.sirfin.demo;
+package it.sirfin.it.sirfin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
